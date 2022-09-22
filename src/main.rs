361af721ccfad1910cacdddacead01b237fc80ed
f1nzo <finwitherspoon@protonmail.com>
2022-09-22 13:02:48 -0700

    h
    Co-authored-by: Ajinkya Gokule <ajinkyagokule@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,35 @@
+use turtle::Turtle;
+
 fn main() {
     let table = Table::new();
     let start = Point {
-        x: 4.0,
-        y: 4.0,
+        x: 16.0,
+        y: 16.0,
     };
     let target = Point {
         x: 0.0,
         y: 50.5
     };
-    let sucess = table.hit(start, target);
+    let success = table.hit(start, target, true);
+    println!("Success: {}", success);
 }
 
 struct Table {
     // In inches
-    height: f32,
-    width: f32,
-    goal_size: f32,
-    x_landmarks: Vec<f32>,
-    y_landmarks: Vec<f32>,
+    height: f64,
+    width: f64,
+    goal_size: f64,
+    x_landmarks: Vec<f64>,
+    y_landmarks: Vec<f64>,
     inner_landmarks: Vec<Point>,
 }
 
 impl Table {
     fn new() -> Table {
 
-        let x_landmarks = vec![4.0, 14.0, 31.0, 33.0, 41.0,];
+        let x_landmarks: Vec<f64> = vec![4.0, 14.0, 31.0, 33.0, 41.0,];
 
-        let y_landmarks = vec![4.0, 23.0, 42.5, 46.5, 50.5, 70.0, 90.0,];
+        let y_landmarks: Vec<f64> = vec![4.0, 23.0, 42.5, 46.5, 50.5, 70.0, 90.0];
 
         let mut inner_landmarks: Vec<Point> = vec![];
 
@@ -51,7 +54,7 @@ impl Table {
         };
     }
 
-    fn hit(&self, start: Point, target: Point) -> bool {
+    fn hit(&self, start: Point, target: Point, draw: bool) -> bool {
         let h;
         let w;
         if target.x < start.x {
@@ -75,17 +78,71 @@ impl Table {
             return false;
         }
 
-        let angle = (w/h).tanh();
+        let angle: f64 = (w/h).tanh();
 
-        println!("height: {h}");
-        println!("width: {w}");
+        let h2 = self.height - target.y;
+        let ratio = h2/h;
+        let w2 = w * ratio;
+
+        let end = Point {
+            x: w2,
+            y: self.height,
+        };
+
+        let goal_start = Point {
+            x: self.width/2.0 - self.goal_size/2.0,
+            y: self.height,
+        };
+
+        let goal_end = Point {
+            x: self.width/2.0 + self.goal_size/2.0,
+            y: self.height,
+        };
+
+        println!("height1: {h}");
+        println!("width1: {w}");
         println!("angle: {angle}");
+        println!("ratio: {ratio}");
+        println!("height2: {h2}");
+        println!("width2: {w2}");
 
-        true
+        if draw {
+            let mut turtle = Turtle::new();
+            // use turtle to draw the table
+            turtle.set_speed("instant");
+            turtle.set_pen_size(2.0);
+            turtle.set_pen_color("black");
+            turtle.set_fill_color("black");
+            turtle.pen_up();
+            turtle.go_to([start.x, start.y]);
+            turtle.pen_down();
+            turtle.go_to([target.x, target.y]);
+            turtle.pen_up();
+            turtle.go_to([end.x, end.y]);
+            turtle.pen_down();
+            turtle.go_to([goal_start.x, goal_start.y]);
+            turtle.go_to([goal_end.x, goal_end.y]);
+            turtle.go_to([end.x, end.y]);
+            turtle.pen_up();
+            turtle.set_pen_size(1.0);
+            turtle.set_pen_color("red");
+            turtle.set_fill_color("red");
+            turtle.set_pen_size(2.0);
+            turtle.set_pen_color("black");
+            turtle.set_fill_color("black");
+            turtle.go_to([0.0, 0.0]);
+            turtle.pen_down();
+        }
+
+        if end.x > goal_start.x && end.x < goal_end.x {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
 
 struct Point {
-    x: f32,
-    y: f32,
+    x: f64,
+    y: f64,
 }
